ed05fddd7f890a34f758970cacb51e592f0bbda3
Nicolas Kion <nicolaskion07@gmail.com>
2025-07-12 10:37:01 +0200

    fix: preserve state

--- resources/js/composables/useWaypoint.ts
+++ resources/js/composables/useWaypoint.ts
@@ -2,12 +2,20 @@ import { router } from '@inertiajs/vue3';
 
 export function useWaypoint() {
     function setWaypoint(character_id: number, solarsystem_id: number, clear_other_waypoints: boolean = true, add_to_beginning: boolean = false) {
-        router.post(route('waypoints.store'), {
-            character_id,
-            destination_id: solarsystem_id,
-            clear_other_waypoints,
-            add_to_beginning,
-        });
+        router.post(
+            route('waypoints.store'),
+            {
+                character_id,
+                destination_id: solarsystem_id,
+                clear_other_waypoints,
+                add_to_beginning,
+            },
+            {
+                preserveState: true,
+                preserveScroll: true,
+                only: ['notification'],
+            },
+        );
     }
 
     return setWaypoint;
