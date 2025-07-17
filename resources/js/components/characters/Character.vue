f623a19b3c4f16f63ec2df501d7a2caed4746119
Nicolas Kion <nicolaskion07@gmail.com>
2025-07-17 09:55:48 +0200

    feat: show docked & inactive indicator

--- resources/js/components/characters/Character.vue
+++ resources/js/components/characters/Character.vue
@@ -18,6 +18,16 @@ const map_solarsystem = computed(() => {
     return map_solarsystems.value.find((solarsystem) => solarsystem.solarsystem_id === character.status?.solarsystem_id);
 });
 
+const is_docked = computed(() => {
+    return !!(character.status?.structure_id || character.status?.station_id);
+});
+
+const is_inactive = computed(() => {
+    if (is_docked.value) return true;
+
+    return character.status?.ship_type?.name === 'Capsule';
+});
+
 function onHover(hovered: boolean) {
     if (map_solarsystem.value) {
         setHoveredMapSolarsystem(map_solarsystem.value.id, hovered);
@@ -26,7 +36,7 @@ function onHover(hovered: boolean) {
 </script>
 
 <template>
-    <TableRow ref="row" v-element-hover="onHover">
+    <TableRow ref="row" v-element-hover="onHover" :data-inactive="is_inactive" class="data-[inactive=true]:opacity-20">
         <TableCell>
             <div class="flex gap-2">
                 <CharacterImage :character_id="character.id" :character_name="character.name" class="size-6 rounded-lg" />
@@ -58,9 +68,11 @@ function onHover(hovered: boolean) {
             <span v-if="map_solarsystem && map_solarsystem.alias" class="flex gap-2">
                 <span>{{ map_solarsystem.alias }}</span>
                 <span class="text-muted-foreground"> {{ map_solarsystem.name }}</span>
+                <span v-if="is_docked" class="text-xs text-muted-foreground">(Docked)</span>
             </span>
             <span v-else-if="character.status?.solarsystem">
                 <span>{{ character.status.solarsystem.name }}</span>
+                <span v-if="is_docked" class="text-xs text-muted-foreground">(Docked)</span>
             </span>
             <span v-else>Unknown Location</span>
         </TableCell>
