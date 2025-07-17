ffae2c7895b365f302fc6eec0a5ea1f707a407d2
Nicolas Kion <nicolaskion07@gmail.com>
2025-07-17 18:41:51 +0200

    feat: add pin input

---
+++ resources/js/components/ui/pin-input/PinInputSeparator.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { Primitive, type PrimitiveProps, useForwardProps } from 'reka-ui';
+
+const props = defineProps<PrimitiveProps>();
+const forwardedProps = useForwardProps(props);
+</script>
+
+<template>
+    <Primitive
+        data-slot="pin-input-separator"
+        v-bind="forwardedProps"
+        class="w-4 text-center"
+    >
+        <slot>
+            -
+        </slot>
+    </Primitive>
+</template>
