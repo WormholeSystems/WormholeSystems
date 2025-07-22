67bfa193938eb5c76163eaa689e8d892ef9de493
Nicolas Kion <nicolaskion07@gmail.com>
2025-07-22 14:50:58 +0200

    Feat: Add option to add solarsystem via context menu

--- resources/js/components/map/MapSolarsystemAdd.vue
+++ resources/js/components/map/MapSolarsystemAdd.vue
@@ -1,23 +1,15 @@
 <script setup lang="ts">
-import SolarsystemClass from '@/components/SolarsystemClass.vue';
 import SolarsystemEffect from '@/components/map/SolarsystemEffect.vue';
-import { Button } from '@/components/ui/button';
+import SolarsystemClass from '@/components/SolarsystemClass.vue';
 import { Combobox, ComboboxAnchor, ComboboxEmpty, ComboboxGroup, ComboboxInput, ComboboxItem, ComboboxList } from '@/components/ui/combobox';
-import { ContextMenuContent, ContextMenuItem, ContextMenuSeparator } from '@/components/ui/context-menu';
-import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle } from '@/components/ui/dialog';
-import { useMapAction, useMapSolarsystems } from '@/composables/map';
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle } from '@/components/ui/dialog';
+import { useMapAction } from '@/composables/map';
 import { useSearch } from '@/composables/useSearch';
 import { TShowMapProps } from '@/pages/maps';
 import { AppPageProps } from '@/types';
 import { TSolarsystem } from '@/types/models';
 import { usePage } from '@inertiajs/vue3';
 import { computed, ref } from 'vue';
-
-const { removeAllMapSolarsystems, sortMapSolarsystemsByRegion, organizeMapSolarsystems, removeSelectedMapSolarsystems } = useMapAction();
-
-const { map_solarsystems_selected } = useMapSolarsystems();
-
-const is_clearing_map = ref(false);
 
 const { addMapSolarsystem } = useMapAction();
 
@@ -41,49 +33,12 @@ const existing_solarsystems = computed(() => {
 
 function handleSolarsystemSelect(solarsystem: TSolarsystem) {
     addMapSolarsystem(solarsystem.id);
-    adding.value = false;
-}
-
-function handleDelete() {
-    is_clearing_map.value = true;
-}
-
-function handleConfirmDelete() {
-    removeAllMapSolarsystems();
-    is_clearing_map.value = false;
-}
-
-function handeCancelDelete() {
-    is_clearing_map.value = false;
 }
 </script>
 
 <template>
-    <ContextMenuContent>
-        <ContextMenuItem @select="adding = true"> Add Solarsystem</ContextMenuItem>
-        <template v-if="map_solarsystems_selected.length">
-            <ContextMenuItem @select="removeSelectedMapSolarsystems"> Remove solarsystems</ContextMenuItem>
-            <ContextMenuItem @select="sortMapSolarsystemsByRegion"> Sort by region</ContextMenuItem>
-            <ContextMenuItem @select="organizeMapSolarsystems"> Organize solarsystems</ContextMenuItem>
-            <ContextMenuSeparator />
-        </template>
-        <ContextMenuItem @select="handleDelete"> Clear map</ContextMenuItem>
-    </ContextMenuContent>
-    <Dialog v-model:open="is_clearing_map">
-        <DialogContent>
-            <DialogHeader>
-                <DialogTitle> Clear map</DialogTitle>
-                <DialogDescription>
-                    Are you sure you want to clear the map? This will remove all solarsystems that are not pinned.
-                </DialogDescription>
-            </DialogHeader>
-            <DialogFooter>
-                <Button variant="outline" @click="handeCancelDelete"> Cancel</Button>
-                <Button variant="destructive" @click="handleConfirmDelete"> Clear map</Button>
-            </DialogFooter>
-        </DialogContent>
-    </Dialog>
     <Dialog v-model:open="adding">
+        <slot />
         <DialogContent>
             <DialogHeader>
                 <DialogTitle> Add Solarsystem</DialogTitle>
