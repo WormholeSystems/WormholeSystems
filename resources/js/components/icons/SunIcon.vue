09421c2b76c88216d109d92ab699f0d167e352b2
Nicolas Kion <nicolaskion07@gmail.com>
2025-07-28 20:26:47 +0200

    Feat: Update SunIcon to use faSunBright for improved visual representation

--- resources/js/components/icons/SunIcon.vue
+++ resources/js/components/icons/SunIcon.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
-import { faSun } from '@awesome.me/kit-659476acbd/icons/sharp/light';
+import { faSunBright } from '@awesome.me/kit-659476acbd/icons/sharp/light';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 </script>
 
 <template>
-    <FontAwesomeIcon :icon="faSun" />
+    <FontAwesomeIcon :icon="faSunBright" />
 </template>
 
 <style scoped></style>
