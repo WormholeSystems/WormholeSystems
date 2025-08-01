caabd42c823b47b1db17cf1a428155eb05776832
Nicolas Kion <nicolaskion07@gmail.com>
2025-08-01 19:10:18 +0200

    Refactor: Create new landing page

---
+++ resources/js/components/icons/BrainIcon.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { faBrainCircuit } from '@awesome.me/kit-659476acbd/icons/sharp/light';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+</script>
+
+<template>
+    <FontAwesomeIcon :icon="faBrainCircuit" />
+</template>
+
+<style scoped></style>
---
+++ resources/js/components/icons/SignatureIcon.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { faSignal } from '@awesome.me/kit-659476acbd/icons/sharp/light';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+</script>
+
+<template>
+    <FontAwesomeIcon :icon="faSignal" />
+</template>
+
+<style scoped></style>
