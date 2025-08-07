ac854108e5ca641bb25021a6d4afe54ab71f3baf
Nicolas Kion <nicolaskion07@gmail.com>
2025-08-07 19:41:18 +0200

    Feat: Update SignatureTypeSelection to disable select when category is not set

--- resources/js/components/signatures/SignatureTypeSelection.vue
+++ resources/js/components/signatures/SignatureTypeSelection.vue
@@ -2,7 +2,7 @@
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 import { ref } from 'vue';
 
-defineProps<{
+const { options } = defineProps<{
     options: string[];
     category: string | null;
     can_write: boolean;
@@ -16,9 +16,9 @@ const open = ref(false);
 </script>
 
 <template>
-    <Select v-model:model-value="model" :disabled="!can_write" v-model:open="open">
+    <Select v-model:model-value="model" :disabled="!can_write || !category" v-model:open="open">
         <SelectTrigger class="col-span-2 w-full overflow-hidden">
-            <SelectValue placeholder="Type" />
+            <SelectValue>{{ model ?? 'Type' }}</SelectValue>
         </SelectTrigger>
         <SelectContent v-if="category">
             <template v-if="open">
