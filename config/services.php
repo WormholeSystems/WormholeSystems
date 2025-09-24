d1c81b87d332674052117c54996e9b4fcd3eab40
Nicolas Kion <nicolaskion07@gmail.com>
2025-09-24 14:39:06 +0200

    Chore: Update project name and description in composer.json and package-lock.json

--- config/services.php
+++ config/services.php
@@ -43,7 +43,7 @@ return [
     'eveonline' => [
         'client_id' => env('EVE_CLIENT_ID'),
         'client_secret' => env('EVE_CLIENT_SECRET'),
-        'redirect' => env('EVE_CALLBACK', 'https://tunnelvision.test/eve/callback'),
+        'redirect' => env('EVE_CALLBACK', 'https://wormholesystems.test/eve/callback'),
     ],
     'discord' => [
         'invite' => env('DISCORD_INVITE', ''),
