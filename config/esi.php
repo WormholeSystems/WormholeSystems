a04a60e8002e8e1f8b119d54eeb2ed343e15bc84
TimKunze96 <TimKunze96@github.com>
2025-09-29 16:27:43 +0200

    Chore: Add CONTACT_EMAIL to .env.example and update user agent configuration

--- config/esi.php
+++ config/esi.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 // config for NicolasKion/Esi
 return [
-    'user_agent' => env('APP_NAME'),
+    'user_agent' => env('APP_USER_AGENT'),
     'client_id' => env('EVE_CLIENT_ID'),
     'client_secret' => env('EVE_CLIENT_SECRET'),
     'retry_policy' => [
