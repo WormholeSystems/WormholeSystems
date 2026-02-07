3f54e7977c7f5e5b262580d554ddd2f1da210ee5
Nicolas Kion <nicolaskion07@gmail.com>
2026-02-07 11:06:39 +0100

    Fix styling

--- resources/js/components/map-killmails/Killmail.vue
+++ resources/js/components/map-killmails/Killmail.vue
@@ -80,87 +80,87 @@ const finalBlow = computed(() => killmail.data.attackers.find((a) => a.final_blo
                 class="col-span-full grid grid-cols-subgrid items-center border-b border-border/30 px-3 py-1.5 hover:bg-muted/30"
                 v-element-hover="onHover"
             >
-            <!-- Victim info -->
-            <a
-                v-if="killmail.ship_type"
-                class="block size-5"
-                :href="`https://zkillboard.com/kill/${killmail.id}/`"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                <TypeImage class="size-5 rounded" :type_id="killmail.ship_type.id" :type_name="killmail.ship_type.name" />
-            </a>
-            <QuestionIcon v-else class="size-5 rounded text-muted-foreground" />
+                <!-- Victim info -->
+                <a
+                    v-if="killmail.ship_type"
+                    class="block size-5"
+                    :href="`https://zkillboard.com/kill/${killmail.id}/`"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <TypeImage class="size-5 rounded" :type_id="killmail.ship_type.id" :type_name="killmail.ship_type.name" />
+                </a>
+                <QuestionIcon v-else class="size-5 rounded text-muted-foreground" />
 
-            <a
-                class="block size-5"
-                :href="`https://zkillboard.com/character/${killmail.data.victim.character_id}/`"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                <VictimImage class="size-5 overflow-hidden rounded" :victim="killmail.data.victim" />
-            </a>
+                <a
+                    class="block size-5"
+                    :href="`https://zkillboard.com/character/${killmail.data.victim.character_id}/`"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <VictimImage class="size-5 overflow-hidden rounded" :victim="killmail.data.victim" />
+                </a>
 
-            <AllianceLogo
-                v-if="killmail.victim_alliance"
-                class="size-5 overflow-hidden rounded"
-                :alliance_id="killmail.victim_alliance.id"
-                :alliance_name="killmail.victim_alliance.name"
-            />
-            <CorporationLogo
-                v-else-if="killmail.victim_corporation"
-                class="size-5 overflow-hidden rounded"
-                :corporation_id="killmail.victim_corporation.id"
-                :corporation_name="killmail.victim_corporation.name"
-            />
-            <span v-else />
+                <AllianceLogo
+                    v-if="killmail.victim_alliance"
+                    class="size-5 overflow-hidden rounded"
+                    :alliance_id="killmail.victim_alliance.id"
+                    :alliance_name="killmail.victim_alliance.name"
+                />
+                <CorporationLogo
+                    v-else-if="killmail.victim_corporation"
+                    class="size-5 overflow-hidden rounded"
+                    :corporation_id="killmail.victim_corporation.id"
+                    :corporation_name="killmail.victim_corporation.name"
+                />
+                <span v-else />
 
-            <span v-if="victimTicker" class="truncate font-mono text-[10px] text-muted-foreground">[{{ victimTicker }}]</span>
-            <span v-else />
+                <span v-if="victimTicker" class="truncate font-mono text-[10px] text-muted-foreground">[{{ victimTicker }}]</span>
+                <span v-else />
 
-            <!-- Location -->
-            <SolarsystemClass :wormhole_class="staticSolarsystem.class" :security="staticSolarsystem.security" class="justify-self-center" />
+                <!-- Location -->
+                <SolarsystemClass :wormhole_class="staticSolarsystem.class" :security="staticSolarsystem.security" class="justify-self-center" />
 
-            <span v-if="map_solarsystem?.alias" class="truncate font-mono text-xs font-medium">{{ map_solarsystem.alias }}</span>
-            <span v-else class="truncate font-mono text-xs">{{ staticSolarsystem.name }}</span>
+                <span v-if="map_solarsystem?.alias" class="truncate font-mono text-xs font-medium">{{ map_solarsystem.alias }}</span>
+                <span v-else class="truncate font-mono text-xs">{{ staticSolarsystem.name }}</span>
 
-            <span class="truncate text-[10px] text-muted-foreground">{{ staticSolarsystem.region?.name }}</span>
+                <span class="truncate text-[10px] text-muted-foreground">{{ staticSolarsystem.region?.name }}</span>
 
-            <!-- Attacker info -->
-            <a
-                v-if="finalBlow?.ship_type_id"
-                class="block size-5"
-                :href="`https://zkillboard.com/kill/${killmail.id}/`"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                <TypeImage class="size-5 rounded" :type_id="finalBlow.ship_type_id" type_name="Final Blow Ship" />
-            </a>
-            <span v-else />
+                <!-- Attacker info -->
+                <a
+                    v-if="finalBlow?.ship_type_id"
+                    class="block size-5"
+                    :href="`https://zkillboard.com/kill/${killmail.id}/`"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <TypeImage class="size-5 rounded" :type_id="finalBlow.ship_type_id" type_name="Final Blow Ship" />
+                </a>
+                <span v-else />
 
-            <a
-                v-if="finalBlow?.character_id"
-                class="block size-5"
-                :href="`https://zkillboard.com/character/${finalBlow.character_id}/`"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                <CharacterImage class="size-5 overflow-hidden rounded" :character_id="finalBlow.character_id" character_name="Final Blow" />
-            </a>
-            <CorporationLogo
-                v-else-if="finalBlow?.corporation_id"
-                class="size-5 overflow-hidden rounded"
-                :corporation_id="finalBlow.corporation_id"
-                corporation_name="Final Blow"
-            />
-            <span v-else />
+                <a
+                    v-if="finalBlow?.character_id"
+                    class="block size-5"
+                    :href="`https://zkillboard.com/character/${finalBlow.character_id}/`"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <CharacterImage class="size-5 overflow-hidden rounded" :character_id="finalBlow.character_id" character_name="Final Blow" />
+                </a>
+                <CorporationLogo
+                    v-else-if="finalBlow?.corporation_id"
+                    class="size-5 overflow-hidden rounded"
+                    :corporation_id="finalBlow.corporation_id"
+                    corporation_name="Final Blow"
+                />
+                <span v-else />
 
-            <span class="text-right font-mono text-[10px] font-medium" :class="attackerCountClass">{{ attackerCount }}</span>
+                <span class="text-right font-mono text-[10px] font-medium" :class="attackerCountClass">{{ attackerCount }}</span>
 
-            <!-- Meta -->
-            <span class="text-right font-mono text-[10px] text-muted-foreground">{{ total_worth }}</span>
+                <!-- Meta -->
+                <span class="text-right font-mono text-[10px] text-muted-foreground">{{ total_worth }}</span>
 
-            <span class="font-mono text-[10px] text-muted-foreground">{{ time_ago }}</span>
+                <span class="font-mono text-[10px] text-muted-foreground">{{ time_ago }}</span>
             </div>
         </DestinationContextMenu>
     </div>
