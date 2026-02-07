bdadc38f160ee300f8a3efe6a4a8c1b0e5e25ba7
Nicolas Kion <nicolaskion07@gmail.com>
2026-02-07 13:39:35 +0100

    Update scheduled commands to include expiration time for overlapping prevention

--- routes/console.php
+++ routes/console.php
@@ -15,8 +15,8 @@ use Illuminate\Queue\Console\PruneBatchesCommand;
 use Illuminate\Support\Facades\Schedule;
 
 Schedule::command(GetServerStatusCommand::class)->runInBackground()->everyMinute()->withoutOverlapping();
-Schedule::command(GetOnlineCharactersCommand::class)->runInBackground()->everyThirtySeconds()->withoutOverlapping()->notDuringDowntime();
-Schedule::command(GetOnlineCharacterLocationsCommand::class)->runInBackground()->everyFiveSeconds()->withoutOverlapping()->notDuringDowntime();
+Schedule::command(GetOnlineCharactersCommand::class)->runInBackground()->everyThirtySeconds()->withoutOverlapping(expiresAt: 60)->notDuringDowntime();
+Schedule::command(GetOnlineCharacterLocationsCommand::class)->runInBackground()->everyFiveSeconds()->withoutOverlapping(expiresAt: 60)->notDuringDowntime();
 Schedule::command(GetSovereigntiesCommand::class)->runInBackground()->daily()->at('15:00')->withoutOverlapping()->notDuringDowntime();
 Schedule::command(GenerateStaticDataCommand::class)->runInBackground()->daily()->withoutOverlapping()->notDuringDowntime();
 Schedule::command(CheckConnectionAgeCommand::class)->runInBackground()->everyTenMinutes()->withoutOverlapping();
