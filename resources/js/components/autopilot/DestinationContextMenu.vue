8670d1f6d3af74befaab7751260a660ea2a00194
Nicolas Kion <nicolaskion07@gmail.com>
2026-03-17 18:40:59 +0100

    Improve context menus with icons and expanded external links
    Add Lucide icons to all context menu items. Expand Dotlan into
    System, Region Map, and Jump Range submenu. Expand zKillboard
    into System and Region. Add gap-2 to ContextMenuSubTrigger. Add
    Copy icon to CopyNameMenu. Fix TransitionGroup warning in
    Character component.

--- resources/js/components/autopilot/DestinationContextMenu.vue
+++ resources/js/components/autopilot/DestinationContextMenu.vue
@@ -15,6 +15,7 @@ import { useNavigationSystems } from '@/composables/useNavigationSystems';
 import usePermission from '@/composables/usePermission';
 import useUser from '@/composables/useUser';
 import { useWaypoint } from '@/composables/useWaypoint';
+import { Compass, MapPin, Navigation, Plus, Route } from 'lucide-vue-next';
 import { computed } from 'vue';
 
 const { solarsystem_id } = defineProps<{
@@ -34,6 +35,7 @@ const already_on_map = computed(() => {
 });
 
 const { canEdit: can_write } = usePermission();
+
 </script>
 
 <template>
@@ -43,7 +45,10 @@ const { canEdit: can_write } = usePermission();
         </ContextMenuTrigger>
         <ContextMenuContent>
             <ContextMenuSub>
-                <ContextMenuSubTrigger>Set destination</ContextMenuSubTrigger>
+                <ContextMenuSubTrigger>
+                    <Navigation class="size-4" />
+                    Set destination
+                </ContextMenuSubTrigger>
                 <ContextMenuSubContent>
                     <ContextMenuItem v-for="character in user.characters" :key="character.id" @select="setWaypoint(character.id, solarsystem_id)">
                         <CharacterImage :character_id="character.id" :character_name="character.name" class="size-5 rounded-lg" />
@@ -53,7 +58,10 @@ const { canEdit: can_write } = usePermission();
             </ContextMenuSub>
 
             <ContextMenuSub>
-                <ContextMenuSubTrigger>Add waypoint</ContextMenuSubTrigger>
+                <ContextMenuSubTrigger>
+                    <MapPin class="size-4" />
+                    Add waypoint
+                </ContextMenuSubTrigger>
                 <ContextMenuSubContent>
                     <ContextMenuItem
                         v-for="character in user.characters"
@@ -65,20 +73,32 @@ const { canEdit: can_write } = usePermission();
                     </ContextMenuItem>
                 </ContextMenuSubContent>
             </ContextMenuSub>
-            <template v-if="!already_on_map && can_write">
-                <ContextMenuSeparator />
-                <ContextMenuItem @select="createMapSolarsystem(solarsystem_id)">Add to map</ContextMenuItem>
-            </template>
-
-            <ContextMenuSeparator />
 
             <ContextMenuSub>
-                <ContextMenuSubTrigger>Route planner</ContextMenuSubTrigger>
+                <ContextMenuSubTrigger>
+                    <Route class="size-4" />
+                    Route planner
+                </ContextMenuSubTrigger>
                 <ContextMenuSubContent>
-                    <ContextMenuItem @select="setFromSystem(solarsystem_id)">Set as origin</ContextMenuItem>
-                    <ContextMenuItem @select="setToSystem(solarsystem_id)">Set as destination</ContextMenuItem>
+                    <ContextMenuItem @select="setFromSystem(solarsystem_id)">
+                        <Compass class="size-4" />
+                        Set as origin
+                    </ContextMenuItem>
+                    <ContextMenuItem @select="setToSystem(solarsystem_id)">
+                        <Navigation class="size-4" />
+                        Set as destination
+                    </ContextMenuItem>
                 </ContextMenuSubContent>
             </ContextMenuSub>
+
+            <template v-if="!already_on_map && can_write">
+                <ContextMenuSeparator />
+                <ContextMenuItem @select="createMapSolarsystem(solarsystem_id)">
+                    <Plus class="size-4" />
+                    Add to map
+                </ContextMenuItem>
+            </template>
+
         </ContextMenuContent>
     </ContextMenu>
 </template>
